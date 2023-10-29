96383fe7f31cf21e6f423bb65eef2c9fa285c818
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-10-29 23:35:52 +0200

    Create 03- arr_sum.cpp

---
+++ 02- Recursion/03- arr_sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int arr_sum(int arr[] , int len)
+{
+    if(len == 1) return arr[0];
+
+    int sum = arr_sum(arr , len - 1);
+
+    return sum + arr[len - 1];
+}
+int main() {
+
+    int arr[] = { 1 , 8 ,2 ,10 , 3};
+
+    cout<<arr_sum(arr , 4)<<endl;
+
+    return 0;
+}
