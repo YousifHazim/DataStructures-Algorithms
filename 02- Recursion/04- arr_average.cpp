5d3154bf2046af184c8aee0844338ac761de8389
Yousif Hazim <105172397+YousifHazim@users.noreply.github.com>
2023-10-29 23:43:05 +0200

    Update 04- arr_average.cpp

--- 02- Recursion/04- arr_average.cpp
+++ 02- Recursion/04- arr_average.cpp
@@ -5,7 +5,8 @@ double arr_average(int arr[] , int len)
 {
     if(len == 1) return arr[0];
 
-    int average = arr_average(arr , len - 1);
+    double average = arr_average(arr , len - 1);
+    average = average * (len-1);
 
     return (average + arr[len - 1]) / len;
 }
@@ -13,7 +14,8 @@ int main() {
 
     int arr[] = { 1 , 8 ,2 ,10 , 3};
 
-    cout<<arr_average(arr , 4)<<endl;
+    cout<<arr_average(arr , 5)<<endl;
 
     return 0;
 }
+
